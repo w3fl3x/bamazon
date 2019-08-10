906025cba7eff18f9146c30a3667aa8eb976c7ee
Huy Nguyen <huy.m.nguyen@live.com>
2019-08-09 23:28:26 -0700

    Add 10 mock products to table in bamazon_db

--- bamazon_db.sql
+++ bamazon_db.sql
@@ -12,3 +12,16 @@ CREATE TABLE products (
     stock_quantity INTEGER(10) default 0,
     PRIMARY KEY (item_id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES
+	('Hot Wheels', 'Toys', 1.00, 275),
+    ('Cinnamon Toast Crunch', 'Food', 3.50, 200),
+    ('Beef Jerky', 'Food', 7.00, 300),
+    ('Sunglasses', 'Fashion', 250, 50),
+    ('Hand Soap', 'Personal Care', 5.50, 70),
+    ('Socks', 'Fashion', 3.25, 83),
+    ('PlayStation 4', 'Electronics', 600, 25),
+    ('Bluetooth Headphones', 'Electronics', 120, 43),
+    ('Lego', 'Toys', 40, 77),
+    ('Paper', 'Office',  8.50, 55);
